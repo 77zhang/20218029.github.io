6fd1d133111acfb5818bc1d3289ceeee8f91e888
20218029리양 <89824329+20218029@users.noreply.github.com>
2021-11-01 20:32:36 +0800

    Add files via upload

---
+++ homework3-2.php
@@ -0,0 +1,32 @@
+<?php
+ header("Content-type:text/html;charset=utf-8");
+
+function Factorial($n) {
+
+    $i = 1;
+
+    $sum=1;
+
+    while($i<=$n){
+
+        $sum*=$i;
+
+        $i++;
+
+    }
+
+    echo "$n 的阶乘为： " . $sum."<br><br>";
+
+}
+
+ 
+
+Factorial(1);
+
+Factorial(2);
+
+Factorial(5);
+
+Factorial(11);
+
+?>
